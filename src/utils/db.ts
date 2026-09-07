ba55d2500ed3afaca084f67df16b7e3bd16326eb
Uwem Udoh <slimudoh@yahoo.com>
2026-09-07 11:05:33 +0100

    updated

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,24 +1,29 @@
 import { Sequelize } from "sequelize";
-
-let DB_NAME = "tutorshub";
-let DB_USER = "root";
-let DB_PASS = "Obalende3783*";
-let DB_HOST = "localhost";
-let DB_PORT = 3306;
+import fs from "fs";
+import path from "path";
 
 console.log("env", process.env.NODE_ENV);
 
-// if (process.env.NODE_ENV === "production") {
-//   DB_NAME = "flywwjyj_choice-pots";
-//   DB_USER = "flywwjyj";
-//   DB_PASS = "bnWyQ5bX1sgF";
-// }
+const isProduction = process.env.NODE_ENV === "production";
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-  host: DB_HOST,
-  port: Number(DB_PORT),
-  dialect: "mysql",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASS as string,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "mysql",
+    logging: false,
+    ...(isProduction && {
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: true,
+          ca: fs.readFileSync(path.join(__dirname, "../ca.pem")).toString(),
+        },
+      },
+    }),
+  },
+);
 
 export default sequelize;
